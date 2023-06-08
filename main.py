86d5610f7c470415f07288ee5f6befee5098eaef
Ranto <ranto.rakotondrajoa@gmail.com>
2023-06-08 23:12:02 +0200

    Use of dotenv instead of external file for token, writing basic message basic message handling

--- main.py
+++ main.py
@@ -5,33 +5,24 @@ after a session using the fastf1 python package
 Entirely for personal use 
 """
 
-import discord 
+import os
+import discord
+from dotenv import load_dotenv
 
-# importing the token from external file
-with open('token.tok', 'r') as f:
-    TOKEN = f.readline().strip()
+# loading environment
+load_dotenv()
+
+# loading token 
+TOKEN=os.getenv("DISCORD_TOKEN")
 
 # client obj. (~bot) from discord.py
 bot = discord.Client(intents=discord.Intents.default())
-
-# event listener (bot goes online )
-@bot.event 
-async def on_ready():
-    # connected server counter 
-    guild_count = 0
-
-    print("SERVERS CONNECTED: ")
-    for guild in bot.guilds:
-        print(f"server: {guild.id} (name: {guild.name})")
-
-        guild_count += 1
 
 
 # event listener (message sent to a channel)
 @bot.event 
 async def on_message(message):
-    if message.content == "hello":
-        # sending back a message 
-        await message.channel.send("Hey YOU")
+    if message.author.bot:      # ensures message has not been sent by bot
+        return
 
 bot.run(TOKEN)
